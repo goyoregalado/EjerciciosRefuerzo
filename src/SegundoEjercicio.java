808b0d2c7352b5b6934859aee3067fe5e30689da
Goyo Regalado <goyoregalado@gmail.com>
2022-04-19 15:48:45 +0100

    Enunciado vacío

---
+++ src/SegundoEjercicio.java
@@ -0,0 +1,22 @@
+public class SegundoEjercicio {
+    // 1. Crea dos atributos de tipo double sin indicar su ámbito de visibilidad.
+    // deben llamarse "a" y "b"
+
+    // 2. Crea un constructor para esta clase que inicialice los atributos "a" y "b"
+
+    // 3. Crea otro constructor para esta clase que solamente inicialice el atributo "b"
+
+    // 4. En la clase Main, crea un menú con tres opciones:
+    //      1.- Añadir 10 objetos con todos los atributos aleatorios y mostrarlos
+    //      2.- Añadir 10 objetos con el atributo "b" aleatorio y mostrarlos
+    //      3.- Salir
+
+    // 5. Cuando el usuario pulse la opción 1 tendrás que crear 10 objetos con sus
+    // dos atributos aleatorios y almacenarlos en un array de objetos de tipo SegundoEjercicio.
+    // Después tendrás que mostrarlos
+
+    // 6. Cuando el usuario pulse la opción 2 tendrás que crear 10 objetos con su
+    // atributo "b" aleatorio y almacenarlos en un array de objetos de tipo SegundoEjercicio.
+    // Después tendrás que mostrarlos
+
+}
