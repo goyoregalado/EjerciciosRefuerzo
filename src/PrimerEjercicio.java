61d0813c3953d560cba94ab2470319c3678d3c93
Goyo Regalado <goyoregalado@gmail.com>
2022-04-19 13:59:03 +0100

    Enunciado vacío

---
+++ src/PrimerEjercicio.java
@@ -0,0 +1,16 @@
+public class PrimerEjercicio {
+    // 1. Crea dos atributos de tipo int sin indicar su ámbito de visibilidad.
+    // deben llamarse "a" y "b"
+
+    // 3. Crea un constructor para esta clase que inicialice los atributos "a" y "b"
+
+    // 2. Crea otra clase que se denomine "PrimerEjercicioB"
+
+    // 3. En esta nueva clase crea un método que se denomine "ejecutar"
+    // en este método debes crear una instancia de la clase PrimerEjercicio
+    // inicializa los atributos "a" y "b" con los valores 1 y 3 respectivamente.
+    // Una vez creada la instancia muestra los valores mediante el método println.
+
+    // 4. En la clase Main, crea una instancia de la clase "PrimerEjercicioB" y ejecuta
+    // su método "ejecutar".
+}
